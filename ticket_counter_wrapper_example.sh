7b1aa50b67b7181af3da4cc9b96408753c5209f0
Tim Littlemore <tim@tjl2.com>
2012-08-10 11:28:28 +0100

    Forgot to set the variables in the wrapper before bundle exec

--- ticket_counter_wrapper_example.sh
+++ ticket_counter_wrapper_example.sh
@@ -9,4 +9,7 @@ ZENDESK_LOGIN_URL="http://zendesk.engineyard.com/users/XXXX/login"
 
 # Set the path to where you copied the repo
 cd /path/to/ticket_counter && \
+SSO_ACCOUNT_FULL_NAME=$SSO_ACCOUNT_FULL_NAME \
+SSO_EMAIL=$SSO_EMAIL SSO_PASSWORD=$SSO_PASSWORD \
+ZENDESK_LOGIN_URL=$ZENDESK_LOGIN_URL \
 bundle exec ruby ticket_counter.rb
